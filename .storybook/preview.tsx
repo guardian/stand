c18a2b3304b8502a7bf7de533679d9fd0781bd49
Mahesh Makani <mahesh@makani.dev>
2025-08-28 15:47:01 +0100

    feat(stand): add storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,83 @@
+import { Global } from '@emotion/react';
+import { css } from '@emotion/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import type { Preview } from '@storybook/react-vite';
+
+const ASSETS_URL = 'https://interactive.guim.co.uk/fonts/guss-webfonts/';
+
+const Fonts = css`
+	@font-face {
+		font-family: 'Guardian Agate Sans';
+		src: url('${ASSETS_URL}GuardianAgateSans1Web/GuardianAgateSans1Web-Regular.woff2')
+			format('woff2');
+		font-weight: 400;
+		font-style: 'normal';
+		font-display: 'fallback';
+	}
+	@font-face {
+		font-family: 'Guardian Agate Sans';
+		src: url('${ASSETS_URL}GuardianAgateSans1Web/GuardianAgateSans1Web-Bold.woff2')
+			format('woff2');
+		font-weight: 700;
+		font-style: normal;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: 'Guardian Agate Sans';
+		src: url('${ASSETS_URL}GuardianAgateSans1Web/GuardianAgateSans1Web-RegularItalic.woff2')
+			format('woff2');
+		font-weight: 400;
+		font-style: italic;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: 'Guardian Agate Sans';
+		src: url('${ASSETS_URL}GuardianAgateSans1Web/GuardianAgateSans1Web-BoldItalic.woff2')
+			format('woff2');
+		font-weight: 700;
+		font-style: italic;
+		font-display: swap;
+	}
+`;
+
+const globalFont = {
+	fontFamily: '"Guardian Agate Sans", "Arial", sans-serif',
+	fontSize: '13px',
+	lineHeight: '1.2',
+	color: '#292929',
+};
+
+const globalStyles = css({
+	html: {
+		height: '100vh',
+		width: '100vw',
+	},
+	body: {
+		...globalFont,
+		minHeight: '100vh',
+		width: '100vw',
+		margin: '0 auto',
+	},
+});
+
+const GlobalStyles = () => <Global styles={[Fonts, globalStyles]} />;
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+	decorators: [
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- withThemeFromJSXProvider is a valid function
+		withThemeFromJSXProvider({
+			GlobalStyles, // Adds GlobalStyles to all stories
+		}),
+	],
+};
+
+// eslint-disable-next-line import/no-default-export -- storybook expects default
+export default preview;
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import guardian from "@guardian/eslint-config";
 
-export default [
-    {
-        ignores: ["dist"],
-    },
-    ...guardian.configs.recommended,
-    ...guardian.configs.jest,
-    ...guardian.configs.react,
-];
+export default [{
+    ignores: ["dist"],
+}, ...guardian.configs.recommended, ...guardian.configs.jest, ...guardian.configs.react, ...storybook.configs["flat/recommended"]];
