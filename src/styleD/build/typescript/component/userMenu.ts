b648bb85891314d7a5f01a22f24dd939eba5ebce
DBlatcher <david.blatcher@guardian.co.uk>
2026-01-30 11:57:47 +0000

    use spacing and sizing tokens

--- src/styleD/build/typescript/component/userMenu.ts
+++ src/styleD/build/typescript/component/userMenu.ts
@@ -6,12 +6,12 @@ export const componentUserMenu = {
 	container: {
 		backgroundColor: '#dcdcdc',
 		width: '300px',
-		paddingY: '5px',
-		paddingX: '5px',
+		paddingY: '0.25rem',
+		paddingX: '0.25rem',
 	},
 	heading: {
 		fontWeight: 700,
-		fontSize: '1.5em',
+		fontSize: '1.125rem',
 		paddingTop: '0px',
 		paddingRight: '0px',
 		paddingBottom: '0px',
@@ -20,10 +20,10 @@ export const componentUserMenu = {
 	},
 	label: {
 		fontWeight: 700,
-		fontSize: '1.2em',
+		fontSize: '0.875rem',
 		paddingTop: '15px',
 		paddingRight: '0px',
-		paddingBottom: '5px',
+		paddingBottom: '0.375rem',
 		paddingLeft: '0px',
 		textAlign: 'left',
 	},
@@ -32,7 +32,7 @@ export const componentUserMenu = {
 		disabledbackgroundColor: '#dcdcdc',
 		baseBorderColor: '#dcdcdc',
 		selectedBorderColor: '#0d4289',
-		gap: '5px',
+		gap: '0.25rem',
 	},
 };
 export type ComponentUserMenu = typeof componentUserMenu;
