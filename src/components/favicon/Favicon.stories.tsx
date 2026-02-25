2a799c490167a107e62ec3a7eb24de219d03bda1
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-02-25 12:04:39 +0000

    docs: add codesandbox links and fixes

--- src/components/favicon/Favicon.stories.tsx
+++ src/components/favicon/Favicon.stories.tsx
@@ -3,6 +3,16 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { baseColors, baseRadius, baseSizing } from '../..';
 import { Favicon } from './Favicon';
 
+// from source: https://github.com/guardian/csnx/blob/main/libs/%40guardian/source/src/react-components/__generated__/icons/SvgHomeHouseOutlined.tsx
+const HomeSvg = () => (
+	<svg viewBox="-3 -3 30 30" xmlns="http://www.w3.org/2000/svg">
+		<path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M12.36 2.5h-.905l-7.6 7.69-.905.905v.453h.905v9.056l.904.896h5.872v-6.333h2.715V21.5h5.89l.904-.932v-9.02h.905v-.453l-1.357-1.357-.001.005zm5.97 8.447-6.412-6.34-6.254 6.326v8.758h3.158V13.81l.452-.453h5.438l.443.453v5.88h3.176z"
+		/>
+	</svg>
+);
 const meta = {
 	title: 'Stand/Editorial Design System/Components/Favicon',
 	component: Favicon,
@@ -17,9 +27,15 @@ export const Letter = {
 	},
 } satisfies Story;
 
-export const Icon = {
+export const IconSymbol = {
 	args: {
-		icon: 'email',
+		icon: 'home',
+	},
+} satisfies Story;
+
+export const IconSvg = {
+	args: {
+		icon: <HomeSvg />,
 	},
 } satisfies Story;
 
