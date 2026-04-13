a47ce9cb480e353589c70295b6d69988ee0336a4
Mahesh Makani <mahesh@makani.dev>
2026-04-13 09:11:05 +0100

    chore(Checkbox): update documentation and exports

--- src/checkbox.ts
+++ src/checkbox.ts
@@ -1,11 +1,12 @@
 /**
- * Checkbox component entry point
+ * Checkbox and CheckboxGroup components entry point
  *
  * Peer dependencies required to use these components:
  * - `@emotion/react`
  * - `react`
  * - `react-dom`
  * - `typescript`
+ * - `react-aria-components`
  *
  * See the `peerDependencies` section of package.json for compatible versions.
  *
@@ -13,7 +14,11 @@
  * you don't need to install these.
  */
 export { Checkbox } from './components/checkbox/Checkbox';
-export type { CheckboxProps } from './components/checkbox/types';
-export type { CheckboxTheme } from './components/checkbox/styles';
+export { CheckboxGroup } from './components/checkbox/CheckboxGroup';
+export type {
+	CheckboxProps,
+	CheckboxGroupProps,
+} from './components/checkbox/types';
+export type { PartialCheckboxTheme as CheckboxTheme } from './components/checkbox/styles';
 export { componentCheckbox } from './styleD/build/typescript/component/checkbox';
 export type { ComponentCheckbox } from './styleD/build/typescript/component/checkbox';
