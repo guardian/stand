7061fad8867f911a641e21feaa0302c677f68ae9
Lakshmi R Pillai <luxmi.r.pillai@gmail.com>
2026-04-29 15:47:22 +0100

    Refactor code

--- src/components/intended-audience-signifier/IntendedAudienceSignifier.tsx
+++ src/components/intended-audience-signifier/IntendedAudienceSignifier.tsx
@@ -1,3 +1,4 @@
+import type { ReactElement } from 'react';
 import { mergeDeep } from '../../util/mergeDeep';
 import {
 	auFlag,
@@ -13,6 +14,13 @@ import {
 } from './styles';
 import type { IntendedAudienceSignifierProps } from './types';
 
+const sourceIcons: Record<string, ReactElement> = {
+	UK: ukFlag,
+	US: usFlag,
+	AUS: auFlag,
+	Global: globeIcon,
+};
+
 export function IntendedAudienceSignifier({
 	source,
 	intendedAudience,
@@ -23,52 +31,33 @@ export function IntendedAudienceSignifier({
 }: IntendedAudienceSignifierProps) {
 	const mergedTheme = mergeDeep(defaultIntendedAudienceSignifierTheme, theme);
 
-	const getSourceIconElement = (
-		sourceTag: string,
-		intendedAudience: string,
-	) => {
-		if (intendedAudience === 'Global') {
+	const getSourceIconElement = (sourceTag: string, audience: string) => {
+		if (audience === 'Global') {
 			return globeIcon;
 		}
-		if (intendedAudience === "Don't know") {
+		if (audience === "Don't know") {
 			return DontKnowIcon;
 		}
-		switch (sourceTag) {
-			case 'UK':
-				return ukFlag;
-			case 'US':
-				return usFlag;
-			case 'AUS':
-				return auFlag;
-			case 'Global':
-				return globeIcon;
-			default:
-				return DontKnowIcon;
-		}
+		return sourceIcons[sourceTag] ?? DontKnowIcon;
 	};
 
 	const getIntendedAudienceIconElement = (
 		sourceTag: string,
-		intendedAudience: string,
+		audience: string,
 	) => {
-		switch (intendedAudience) {
-			case 'Global':
-				return globeIcon;
-			case 'Domestic for Domestic':
-				return getSourceIconElement(sourceTag, intendedAudience);
-			case 'Domestic For Global':
-				return globeIcon;
-			case "Don't know":
-				return DontKnowIcon;
-			case 'UK':
-				return ukFlag;
-			case 'US':
-				return usFlag;
-			case 'AUS':
-				return auFlag;
-			default:
-				return DontKnowIcon;
+		if (audience === 'Domestic for Domestic') {
+			return getSourceIconElement(sourceTag, audience);
 		}
+
+		if (audience === 'Global' || audience === 'Domestic For Global') {
+			return globeIcon;
+		}
+
+		if (audience === "Don't know") {
+			return DontKnowIcon;
+		}
+
+		return sourceIcons[audience] ?? DontKnowIcon;
 	};
 
 	if (intendedAudience === "Don't know") {
