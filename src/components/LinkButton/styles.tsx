3b567a11e785de3300f773ce60c05daf0180a1c0
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2026-05-20 16:43:25 +0100

    sort out file tracking

--- src/components/LinkButton/styles.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonTheme, PartialButtonTheme } from '../Button/styles';
-import { buttonStyles, defaultButtonTheme } from '../Button/styles';
-
-export type LinkButtonTheme = ButtonTheme;
-export type PartialLinkButtonTheme = PartialButtonTheme;
-
-export const defaultLinkButtonTheme: ButtonTheme = defaultButtonTheme;
-
-export const linkButtonStyles = buttonStyles;
