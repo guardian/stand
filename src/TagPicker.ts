cfce9e7908351b998db76cf321f5f52b4d7603f7
DBlatcher <david.blatcher@guardian.co.uk>
2026-06-09 16:58:41 +0100

    organise props and annotations, add TagPicker to component export file

--- src/TagPicker.ts
+++ src/TagPicker.ts
@@ -16,7 +16,10 @@
 export { Autocomplete } from './components/TagPicker/Autocomplete';
 export { TagAutocomplete } from './components/TagPicker/TagAutocomplete';
 export { TagTable } from './components/TagPicker/TagTable';
+export { TagPicker } from './components/TagPicker/TagPicker';
 export { componentAutocomplete } from './styleD/build/typescript/component/autocomplete';
 export type { ComponentAutocomplete } from './styleD/build/typescript/component/autocomplete';
 export { componentTagTable } from './styleD/build/typescript/component/tagTable';
 export type { ComponentTagTable } from './styleD/build/typescript/component/tagTable';
+export { componentTagPicker } from './styleD/build/typescript/component/tagPicker';
+export type { ComponentTagPicker } from './styleD/build/typescript/component/tagPicker';
--- src/components/TagPicker/TagPicker.tsx
+++ src/components/TagPicker/TagPicker.tsx
@@ -7,104 +7,120 @@ import type { ComponentAutocomplete, ComponentTagTable } from '../../TagPicker';
 import type { DeepPartial } from '../../util/types';
 import { tagPickerStyles } from './styles';
 import { TagSelectWithTypes } from './TagSelectWithTypes';
-import type { TagTableProps } from './TagTable';
 import { TagTable } from './TagTable';
 import type { FilterOption, TagRow } from './types';
 
 export type TagPickerProps<T extends TagRow = TagRow> = {
+	/** `addTag` - Function called when a tag is selected from the dropdown or proposal list */
 	addTag: (tag: T) => void;
+	/** `loading` - Whether the component is loading options for the dropdown */
 	loading: boolean;
+	/** `offline` - Whether the component has failed to fetch a list of options, indicating either that the service is offline or cannot be reached */
 	offline?: boolean;
-	onReorder: TagTableProps<T>['onReorder'];
-	onSearch: { (queryText: string, tagTypeFilter?: string): void };
+	/** `onReorder -  Function called when a selected tag is moved in the tag table */
+	onReorder: (tags: T[]) => void;
+	/** `onReorder -  Function called the user enters a query or selects a tag type filter */
+	onSearch: (queryText: string, tagTypeFilter?: string) => void;
+	/** `options` - The list of options shown in the dropdown */
 	options: T[];
+	/** `proposedTags` - A list of "proposed tags" that the user can add */
 	proposedTags: T[];
+	/** `readOnly` - Whether the interactions modifying the list of tags shoudl be prevented */
 	readOnly?: boolean;
-	removeTag: TagTableProps<T>['removeAction'];
+	/** `addTag` - Function called when a tag is removed from selected tags list */
+	removeTag: (tag: T) => void;
+	/** `retryConnection -  Function called to check if the service to fetch tag options is available again */
 	retryConnection?: () => void;
+	/** `tags` - The list of selected tags */
 	tags: T[];
+	/** `canRemove -  A filter function that determines if the user is able to remove the tag from the selected tags */
+	canRemove?: (tag: T) => boolean;
+	/** `filterRows -  A filter function that determines if the tag should be excluded from the selected and proposed tag tables  */
+	filterRows?: (tag: T) => boolean;
+	/** `tagTypes` - the list of options for filtering the search by tag type */
+	tagTypes: FilterOption[];
+	/** `offlineBackupTags` - A list of tags that the user can select if the service is offline */
+	offlineBackupTags?: T[];
 
 	highlightLeadingTag?: boolean;
 	searchPlaceholder?: string;
 	searchLabel?: string;
-	filterRows?: { (tag: T): boolean };
-	tagTypes: FilterOption[];
-	offlineBackupTags?: T[];
-	canRemove: TagTableProps<T>['canRemove'];
 	removeIcon: ReactElement;
 	searchIcon?: ReactElement;
+	showTagType?: boolean;
+	showTagSectionName?: boolean;
 
+	/** `theme` - Used to customise the look and feel of the TagPicker component */
+	theme?: DeepPartial<ComponentTagPicker>;
 	tagTableTheme?: DeepPartial<ComponentTagTable>;
 	proposedTagTableTheme?: DeepPartial<ComponentTagTable>;
 	autoCompleteTheme?: DeepPartial<ComponentAutocomplete>;
 	selectTheme?: DeepPartial<ComponentSelect>;
 
-	showTagType?: boolean;
-	showTagSectionName?: boolean;
-
-	/** `theme` - Used to customise the look and feel of the TagTable component */
-	theme?: DeepPartial<ComponentTagPicker>;
 	/** `cssOverrides` - Escape hatch for styling that doesn't fall into the theme */
 	cssOverrides?: SerializedStyles;
 };
 
-function filterOutSelectedTags<T extends TagRow = TagRow>(
-	tagList: T[],
-	selectedTags: T[],
-) {
-	const selectedTagIds = selectedTags.map(({ id }) => id);
-	return tagList.filter(({ id }) => !selectedTagIds.includes(id));
-}
-
+/**
+ * ## TagPicker
+ *
+ * *Status: Testing*
+ *
+ * **Peer dependencies:**
+ * - `react-aria-components`
+ *
+ * See the `peerDependencies` section of the `package.json` for compatible versions to install.
+ *
+ *
+ * #### Props
+ *
+ * See {@link TagPickerProps} for a full list of props and descriptions.
+ *
+ * #### Example
+ *
+ * This is currently still in testing phase, so a production implementation is not yet available.
+ */
 export function TagPicker<T extends TagRow = TagRow>({
-	tags,
-	onSearch,
-	tagTypes,
-	filterRows,
-
-	searchLabel = 'Search for tags',
-	searchPlaceholder = 'Search for tags',
-
 	addTag,
 	loading,
-	options,
-
-	readOnly = false,
 	offline = false,
+	onReorder,
+	onSearch,
+	options,
+	proposedTags,
+	readOnly = false,
+	removeTag,
 	retryConnection,
+	tags,
+	canRemove,
+	filterRows,
+	tagTypes,
 	offlineBackupTags,
 
-	// selected tag table
-	onReorder,
-	canRemove,
-	removeTag,
-	removeIcon,
 	highlightLeadingTag,
-	tagTableTheme,
-
+	searchPlaceholder = 'Search for tags',
+	searchLabel = 'Search for tags',
+	removeIcon,
 	searchIcon,
-
-	// proposed tag table
-	proposedTags,
-
-	proposedTagTableTheme,
-	selectTheme,
-	autoCompleteTheme,
-
-	showTagSectionName,
 	showTagType,
+	showTagSectionName,
 
 	theme,
+	tagTableTheme,
+	proposedTagTableTheme,
+	autoCompleteTheme,
+	selectTheme,
 	cssOverrides,
 }: TagPickerProps<T>) {
-	const showBackupListWhenOffline =
-		!readOnly && offlineBackupTags && offlineBackupTags.length > 0;
-
-	const proposedTagsWithoutSelected = filterOutSelectedTags(proposedTags, tags);
-	const backupTagsWithoutSelected = filterOutSelectedTags(
-		offlineBackupTags ?? [],
-		tags,
+	const selectedTagIds = tags.map(({ id }) => id);
+	const proposedTagsWithoutSelected = proposedTags.filter(
+		({ id }) => !selectedTagIds.includes(id),
 	);
+	const backupTagsWithoutSelected =
+		offlineBackupTags?.filter(({ id }) => !selectedTagIds.includes(id)) ?? [];
+
+	const showBackupListWhenOffline =
+		!readOnly && offlineBackupTags && backupTagsWithoutSelected.length > 0;
 
 	return (
 		<div css={[tagPickerStyles(theme), cssOverrides]}>
