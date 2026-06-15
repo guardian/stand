acfd083a5e416d55d196cf913a44d18c30af2b3e
Liam Duffy <3338808+liamness@users.noreply.github.com>
2026-06-15 12:43:49 +0100

    Adjust title of intended audience signifier to be simpler when source and target are the same

--- src/components/IntendedAudienceSignifier/IntendedAudienceSignifier.tsx
+++ src/components/IntendedAudienceSignifier/IntendedAudienceSignifier.tsx
@@ -21,34 +21,11 @@ const icons = {
 	global: globeIcon,
 };
 
-type Description =
-	| 'Global'
-	| 'Domestic'
-	| 'United Kingdom'
-	| 'United States'
-	| 'Australia';
-
-const descriptionMap: Record<SourceOrTarget, Description> = {
+const descriptionMap = {
 	global: 'Global',
 	uk: 'United Kingdom',
 	us: 'United States',
 	au: 'Australia',
-};
-
-const getSourceDescription = (source: SourceOrTarget) => descriptionMap[source];
-
-const getTargetDescription = (
-	source: SourceOrTarget,
-	target: SourceOrTarget,
-): Description => {
-	switch (target) {
-		case 'global':
-			return 'Global';
-		case 'uk':
-		case 'us':
-		case 'au':
-			return target === source ? 'Domestic' : descriptionMap[source];
-	}
 };
 
 const AUDIENCE_NOT_KNOWN_DESCRIPTION = 'Intended audience unknown';
@@ -59,7 +36,10 @@ const getSignifierTitle = (
 	if (!source || !target) {
 		return AUDIENCE_NOT_KNOWN_DESCRIPTION;
 	}
-	return `${getSourceDescription(source)} for ${getTargetDescription(source, target)} audience`;
+	if (source === target) {
+		return `${descriptionMap[source]} audience`;
+	}
+	return `${descriptionMap[source]} for ${descriptionMap[target]} audience`;
 };
 
 export function IntendedAudienceSignifier({
