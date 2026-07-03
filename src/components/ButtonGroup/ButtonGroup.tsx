67809d8d4af0fc622065361816eb57f4ae2e6a18
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-07-03 17:22:35 +0100

    Add example stories and docs

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,15 +1,35 @@
+import React from 'react';
 import { mergeDeep } from '../../util/mergeDeep';
+import { Button } from '../Button/Button';
+import type { ButtonProps } from '../Button/types';
 import { buttonGroupStyles, defaultButtonGroupTheme } from './styles';
 import type { ButtonGroupProps } from './types';
 
 export const ButtonGroup = ({
+	size = 'md',
 	theme = {},
 	children,
 	cssOverrides,
 }: ButtonGroupProps) => {
 	const mergedTheme = mergeDeep(defaultButtonGroupTheme, theme);
 
+	const buttons: React.ReactElement[] = [];
+
+	// only allow Button components as children of ButtonGroup and apply size prop to them
+	React.Children.forEach(children, (child, index) => {
+		if (!React.isValidElement(child) || child.type !== Button) {
+			return;
+		}
+
+		buttons.push(
+			React.cloneElement(child as React.ReactElement<ButtonProps>, {
+				key: child.key ?? index, // ensure key is set for each child
+				size,
+			}),
+		);
+	});
+
 	return (
-		<div css={[buttonGroupStyles(mergedTheme), cssOverrides]}>{children}</div>
+		<div css={[buttonGroupStyles(mergedTheme), cssOverrides]}>{buttons}</div>
 	);
 };
