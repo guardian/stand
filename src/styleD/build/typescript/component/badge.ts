c6af3b54a1d4a296605dd2695b825d6afb961e8f
Rik Roots <rik.roots@guardian.co.uk>
2026-07-08 16:15:30 +0100

    Update badge.json to match design

--- src/styleD/build/typescript/component/badge.ts
+++ src/styleD/build/typescript/component/badge.ts
@@ -3,9 +3,81 @@
  */
 
 export const componentBadge = {
+	size: {
+		medium: {
+			display: 'inline-flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			padding: {
+				top: '4px',
+				right: '12px',
+				bottom: '4px',
+				left: '12px',
+			},
+			typography: {
+				font: 'normal 700 1rem/1.15 Open Sans',
+				letterSpacing: '-0.03125rem',
+				fontWidth: 95,
+			},
+		},
+		small: {
+			display: 'inline-flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			padding: {
+				top: '4px',
+				right: '8px',
+				bottom: '4px',
+				left: '8px',
+			},
+			typography: {
+				font: 'normal 700 0.875rem/1.15 Open Sans',
+				letterSpacing: '-0.0125rem',
+				fontWidth: 95,
+			},
+		},
+		xSmall: {
+			display: 'inline-flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			padding: {
+				top: '1px',
+				right: '6px',
+				bottom: '3px',
+				left: '6px',
+			},
+			typography: {
+				font: 'normal 700 0.75rem/1.15 Open Sans',
+				letterSpacing: '0rem',
+				fontWidth: 95,
+			},
+		},
+	},
 	color: {
-		background: '#cde4c9',
-		text: '#ff69b4',
+		greenBg: {
+			background: '#aed2a6',
+			color: '#24491d',
+		},
+		yellowBg: {
+			background: '#f1c21b',
+			color: '#000000',
+		},
+		greyBg: {
+			background: '#dcdcdc',
+			color: '#000000',
+		},
+		orangeBg: {
+			background: '#fcddc6',
+			color: '#000000',
+		},
+		purpleBg: {
+			background: '#dac3e8',
+			color: '#000000',
+		},
+		redBg: {
+			background: '#f5c6c0',
+			color: '#000000',
+		},
 	},
 };
 export type ComponentBadge = typeof componentBadge;
