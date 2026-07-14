537dbcb6bbd9ca0e92c2c647cdd2112c9239e894
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-07-14 17:37:46 +0100

    Basic tooltip behaviour and styling

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,11 +1,11 @@
-import { css } from '@emotion/react';
+// import { css } from '@emotion/react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Tooltip } from './Tooltip';
 
 const meta = {
 	title: 'Stand/Tools Design System/Components/Tooltip',
 	component: Tooltip,
-	parameters: {},
+	parameters: { layout: 'centered' },
 } satisfies Meta<typeof Tooltip>;
 
 type Story = StoryObj<typeof Tooltip>;
@@ -14,31 +14,31 @@ export const Default = {
 	args: {},
 } satisfies Story;
 
-export const CustomMessage = {
-	args: {
-		message: 'hi there',
-	},
-} satisfies Story;
+// export const CustomMessage = {
+// 	args: {
+// 		message: 'hi there',
+// 	},
+// } satisfies Story;
 
-export const CustomTheme = {
-	args: {
-		theme: {
-			color: {
-				background: 'blue',
-				text: 'lime',
-			},
-		},
-	},
-} satisfies Story;
+// export const CustomTheme = {
+// 	args: {
+// 		theme: {
+// 			color: {
+// 				background: 'blue',
+// 				text: 'lime',
+// 			},
+// 		},
+// 	},
+// } satisfies Story;
 
-export const CssOverrides = {
-	args: {
-		cssOverrides: css({
-			color: 'red',
-			border: '4px dotted black',
-			padding: 5,
-		}),
-	},
-} satisfies Story;
+// export const CssOverrides = {
+// 	args: {
+// 		cssOverrides: css({
+// 			color: 'red',
+// 			border: '4px dotted black',
+// 			padding: 5,
+// 		}),
+// 	},
+// } satisfies Story;
 
 export default meta;
--- src/components/Tooltip/styles.ts
+++ src/components/Tooltip/styles.ts
@@ -1,18 +1,59 @@
-import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
+import { baseSpacing } from '../../styleD/build/typescript/base/spacing';
 import {
 	componentTooltip,
 	type ComponentTooltip,
 } from '../../styleD/build/typescript/component/tooltip';
+import { semanticColors } from '../../styleD/build/typescript/semantic/colors';
+import { semanticRadius } from '../../styleD/build/typescript/semantic/radius';
+import { semanticSpacing } from '../../styleD/build/typescript/semantic/spacing';
+import { semanticTypography } from '../../styleD/build/typescript/semantic/typography';
 import type { DeepPartial, Prettify } from '../../util/types';
 
 export type TooltipTheme = Prettify<ComponentTooltip>;
 export type PartialTooltipTheme = Prettify<DeepPartial<TooltipTheme>>;
 export const defaultTooltipTheme: TooltipTheme = componentTooltip;
 
-export const tooltipStyles = (theme: TooltipTheme): SerializedStyles => {
-	return css`
-		background-color: ${theme.color.background};
-		color: ${theme.color.text};
-	`;
-};
+export const tooltipTriggerStyles = css`
+	display: inline-flex;
+	appearance: none;
+	border: none;
+	padding: 0;
+	width: 20px;
+	height: 20px;
+	align-items: center;
+	justify-content: center;
+	border-radius: 50%;
+	color: ${semanticColors.text.strong};
+	background-color: ${semanticColors.fill.informationWeak};
+	> span {
+		font-size: 0.875rem;
+	}
+`;
+
+export const tooltipStyles = css`
+	padding: ${semanticSpacing.stackSm};
+	color: ${semanticColors.text.strong};
+	background-color: ${semanticColors.fill.informationWeak};
+	box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.15);
+	border-radius: ${semanticRadius.cornerSm};
+	font: ${semanticTypography.bodyXs.font};
+	letter-spacing: ${semanticTypography.bodyXs.letterSpacing};
+	font-variation-settings: 'wdth' ${semanticTypography.bodyXs.fontWidth};
+
+	&[data-placement='top'] {
+		margin-bottom: ${baseSpacing['6Rem']};
+	}
+
+	&[data-placement='bottom'] {
+		margin-top: ${baseSpacing['6Rem']};
+	}
+
+	&[data-placement='right'] {
+		margin-left: ${baseSpacing['6Rem']};
+	}
+
+	&[data-placement='left'] {
+		margin-right: ${baseSpacing['6Rem']};
+	}
+`;
