5acf7437d12094a14bf7e50e6d4f139a33812aa5
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-08-13 18:02:31 +0100

    Update markup and apply styling and spacing

--- src/components/TextListInput/TextListInput.tsx
+++ src/components/TextListInput/TextListInput.tsx
@@ -1,29 +1,54 @@
 import { mergeDeep } from '../../util/mergeDeep';
+import { Badge } from '../Badge/Badge';
 import { Button } from '../Button/Button';
 import { IconButton } from '../IconButton/IconButton';
+import type { PartialTextInputTheme } from '../TextInput/styles';
 import { TextInput } from '../TextInput/TextInput';
-import { defaultTextListInputTheme, textListInputStyles } from './styles';
+import {
+	defaultTextListInputTheme,
+	inputStyles,
+	labelStyles,
+	layoutStyles,
+	textListInputStyles,
+} from './styles';
 import type { TextListInputProps } from './types';
+
+const textInputTheme: PartialTextInputTheme = {
+	shared: {
+		marginTop: '0',
+	},
+};
 
 export const TextListInput = (props: TextListInputProps) => {
 	const mergedTheme = mergeDeep(defaultTextListInputTheme, props.theme ?? {});
 
 	return (
-		<div css={[textListInputStyles(mergedTheme), props.cssOverrides]}>
-			<label>
-				Text input list <span>2 items</span>
-			</label>
-			<TextInput />
-			<IconButton variant="tertiary" size="md" ariaLabel="Remove item">
-				remove_circle_outline
-			</IconButton>
-			<TextInput />
-			<IconButton variant="tertiary" size="md" ariaLabel="Add item">
-				remove_circle_outline
-			</IconButton>
-			<Button variant="tertiary" size="xs" icon="add">
-				Add new item
-			</Button>
-		</div>
+		<fieldset css={[textListInputStyles(mergedTheme), props.cssOverrides]}>
+			<legend css={labelStyles}>
+				Text input list{' '}
+				<Badge color="grey" size="xs">
+					2 items
+				</Badge>
+			</legend>
+			<div css={layoutStyles}>
+				<div css={inputStyles}>
+					<TextInput theme={textInputTheme} />
+					<IconButton variant="tertiary" size="md" ariaLabel="Remove item">
+						remove_circle_outline
+					</IconButton>
+				</div>
+				<div css={inputStyles}>
+					<TextInput theme={textInputTheme} />
+					<IconButton variant="tertiary" size="md" ariaLabel="Remove item">
+						remove_circle_outline
+					</IconButton>
+				</div>
+				<div>
+					<Button variant="tertiary" size="xs" icon="add">
+						Add new item
+					</Button>
+				</div>
+			</div>
+		</fieldset>
 	);
 };
--- src/components/TextListInput/styles.ts
+++ src/components/TextListInput/styles.ts
@@ -4,7 +4,11 @@ import {
 	componentTextListInput,
 	type ComponentTextListInput,
 } from '../../styleD/build/typescript/component/textListInput';
+import { semanticColors } from '../../styleD/build/typescript/semantic/colors';
+import { semanticSpacing } from '../../styleD/build/typescript/semantic/spacing';
+import { semanticTypography } from '../../styleD/build/typescript/semantic/typography';
 import type { DeepPartial, Prettify } from '../../util/types';
+import { convertTypographyToEmotionStringStyle } from '../../utils';
 
 export type TextListInputTheme = Prettify<ComponentTextListInput>;
 export type PartialTextListInputTheme = Prettify<
@@ -21,3 +25,24 @@ export const textListInputStyles = (
 		color: ${theme.color.text};
 	`;
 };
+
+export const labelStyles = css`
+	display: flex;
+	align-items: center;
+	gap: ${semanticSpacing.stackXs};
+	color: ${semanticColors.text.strong};
+	${convertTypographyToEmotionStringStyle(semanticTypography.labelFormMd)}
+`;
+
+export const layoutStyles = css`
+	display: flex;
+	flex-direction: column;
+	gap: ${semanticSpacing.stackMd};
+	margin-top: ${semanticSpacing.stackXs};
+`;
+
+export const inputStyles = css`
+	display: flex;
+	align-items: center;
+	gap: ${semanticSpacing.stackXs};
+`;
